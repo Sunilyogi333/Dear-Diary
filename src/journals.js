cd51d1d5ab29ba93aa339bc7ee154a466ed044fd
Sunil Nath <sunil.211443@ncit.edu.np>
2023-07-16 22:29:27 +0545

    frontend completed

---
+++ src/journals.js
@@ -0,0 +1,13 @@
+const textarea= document.querySelector("textarea");
+textarea.addEventListener("keyup", e =>{
+textarea.style.height="80vh";
+let taHeight = e.target.scrollHeight;
+textarea.style.height =`${taHeight}px`;
+});
+
+const content = document.getElementsByClassName("content");
+content.addEventListener("keyup", e=>{
+    textarea.style.height="auto"
+let coHeight = e.target.scrollHeight;
+textarea.style.height=`${coHeight}px`;
+});
