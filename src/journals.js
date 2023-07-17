b6ac9a31649577df081e843649bc9186de2e9ffa
Sunil Nath <sunil.211443@ncit.edu.np>
2023-07-18 00:07:34 +0545

    manage sidebar

--- src/journals.js
+++ src/journals.js
@@ -5,9 +5,13 @@ let taHeight = e.target.scrollHeight;
 textarea.style.height =`${taHeight}px`;
 });
 
-const content = document.getElementsByClassName("content");
+const content = document.getElementsByClassName(".content");
 content.addEventListener("keyup", e=>{
     textarea.style.height="auto"
 let coHeight = e.target.scrollHeight;
 textarea.style.height=`${coHeight}px`;
 });
+document.getElementsByClassName('content').addEventListener('click', function (e) {
+    e.preventDefault();
+    doSomething();
+})
